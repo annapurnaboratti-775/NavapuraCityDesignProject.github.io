91a7ea3acfc0437061cdab72b8fb3ce623505b9e
annapurnaboratti-775 <lalitaboratti@gmail.com>
2025-11-29 11:01:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+// Smooth scroll for the hero button
+document.querySelector(".hero-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.querySelector("#team").scrollIntoView({ behavior: "smooth" });
+});
 
