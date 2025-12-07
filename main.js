8383af373caf28709e69b075a838a7de7447ae37
annapurnaboratti-775 <lalitaboratti@gmail.com>
2025-12-07 19:11:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-// Smooth fade in
-document.addEventListener("DOMContentLoaded", () => {
-    document.body.style.opacity = "1";
+// Smooth scroll for the hero button
+
+document.querySelector(".hero-btn").addEventListener("click", function (e) {
+e.preventDefault();
+document.querySelector("#team").scrollIntoView({ behavior: "smooth" });
 });
